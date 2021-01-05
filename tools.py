70038971e86a7809ea9669efcbc01894638a8958
alexchung <yonganzhong@outlook.com>
2021-01-05 17:42:28 +0800

    add models

---
+++ tools.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : tools.py
+# @ Description:  
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2021/1/5 下午5:34
+# @ Software   : PyCharm
+#------------------------------------------------------
+
+import torch
+import models
+from configs.cfgs import args
+
+
+def get_model(args, cfg=None):
+
+    if cfg is not None:
+        model = models.__dict__[args.arch](dataset=args.dataset, depth=args.depth, cfg=cfg)
+    else:
+        model = models.__dict__[args.arch](dataset=args.dataset, depth=args.depth)
+
+    return model
+
+if __name__ == "__main__":
+
+    model = get_model(args)
+
+    print(model)
